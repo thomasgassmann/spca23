a4b01a17b90c7d9863fa2d39106d2a53c782512d
Thomas Gassmann <tgassmann@student.ethz.ch>
2023-10-21 14:26:33 +0200

    fix qsort
    Signed-off-by: Thomas Gassmann <tgassmann@student.ethz.ch>

--- assignment4/sort.c
+++ assignment4/sort.c
@@ -2,7 +2,47 @@
 
 extern void quicksort(int *x, int *y);
 
+// void swap(int *x, int *y) {
+//     int tmp = *x;
+//     *x = *y;
+//     *y = tmp;
+// }
+
+// int *partition(int *x, int *y) {
+//     int pivot = *y;
+
+//     int *i = x - 1;
+//     for (int *j = x; j <= y - 1; j++) {
+//         if (*j <= pivot) {
+//             i++;
+//             swap(i, j);
+//         }
+//     }
+
+//     i++;
+//     swap(i, y);
+//     return i;
+// }
+
+// void quicksort(int *x, int *y) {
+//     if (x >= y) {
+//         return;
+//     }
+
+//     int *p = partition(x, y);
+//     quicksort(x, p - 1);
+//     quicksort(p + 1, y);
+// }
+
 int main() {
     int arr[7] = {2, 6, 1, 0, 10, 50, 1};
+    for (int i = 0; i < 7; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    printf("Sorting...\n");
     quicksort(&arr[0], &arr[6]);
+    for (int i = 0; i < 7; i++) {
+        printf("%d\n", arr[i]);
+    }
 }
